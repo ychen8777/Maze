7a01ba302f7090f3e520e6123ebf338366b77ab2
ychen <ychen8777@gmail.com>
2020-08-07 23:07:25 -0400

    defined control panel

--- src/MazeFrame.java
+++ src/MazeFrame.java
@@ -10,6 +10,9 @@ public class MazeFrame extends JFrame {
         this.mazeHeight = mazeHeight;
         this.mazeWidth = mazeWidth;
 
+        MazeControl mazeControl = new MazeControl();
+        setContentPane(mazeControl);
+        pack();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
@@ -19,6 +22,28 @@ public class MazeFrame extends JFrame {
 
     public MazeFrame(String title) {
         this(title, 1024, 768 );
+    }
+
+    private class MazeControl extends JPanel {
+        private JLabel widthLabel = new JLabel("Width: ");
+        private JTextField widthField = new JTextField("640");
+        private JLabel heightLabel = new JLabel("Height:");
+        private JTextField heightField = new JTextField("480");
+        private JButton generateMazeButton = new JButton("Generate Maze");
+        private JButton DFSButton = new JButton("Solve by DFS");
+        private JButton BSFButton = new JButton("Solve by BFS");
+
+        public MazeControl(){
+            this.add(widthLabel);
+            this.add(widthField);
+            this.add(heightLabel);
+            this.add(heightField);
+            this.add(generateMazeButton);
+            this.add(DFSButton);
+            this.add(BSFButton);
+
+
+        }
     }
 
 }
