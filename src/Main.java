df6c3ae98dea6b3dae4eaee21890bc4ac63e44c9
ychen <ychen8777@gmail.com>
2020-08-08 22:44:45 -0400

    define generateMazeButton to caputer user inputs in main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         MazeFrame window = new MazeFrame("Maze Demo");
+        JButton generateMazeButton = window.getGenerateMazeButton();
+        generateMazeButton.addActionListener((e) -> {
+            StringBuilder res = new StringBuilder();
+            res.append("Width ");
+            res.append(window.getInputWidth());
+            res.append(", height: ");
+            res.append(window.getInputHeight());
+            System.out.println(res.toString());});
     }
 }
