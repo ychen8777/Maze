8bd0e01778a0d8c28849bfed8f208d42556e0aaa
ychen <ychen8777@gmail.com>
2020-08-08 22:43:22 -0400

    define getters to access TextFields and Buttons from MazeControl

--- src/MazeFrame.java
+++ src/MazeFrame.java
@@ -4,13 +4,14 @@ public class MazeFrame extends JFrame {
 
     private int mazeHeight;
     private int mazeWidth;
+    private MazeControl mazeControl;
 
     public MazeFrame(String title, int mazeWidth, int mazeHeight) {
         super(title);
         this.mazeHeight = mazeHeight;
         this.mazeWidth = mazeWidth;
 
-        MazeControl mazeControl = new MazeControl();
+        this.mazeControl = new MazeControl();
         setContentPane(mazeControl);
         pack();
 
@@ -24,11 +25,34 @@ public class MazeFrame extends JFrame {
         this(title, 1024, 768 );
     }
 
+    public int getInputWidth() {
+        return Integer.parseInt(this.mazeControl.getWidthInput());
+    }
+
+    public int getInputHeight() {
+        return Integer.parseInt(this.mazeControl.getHeightInput());
+    }
+
+    public JButton getGenerateMazeButton() {
+        return this.mazeControl.getGenerateMazeButton();
+    }
+
+    public JButton getDFSButton() {
+        return this.mazeControl.getDFSButton();
+    }
+
+    public JButton getBSFButton() {
+        return this.mazeControl.getBSFButton();
+    }
+
+
+
+
     private class MazeControl extends JPanel {
         private JLabel widthLabel = new JLabel("Width: ");
-        private JTextField widthField = new JTextField("640");
+        private JTextField widthField = new JTextField("103");
         private JLabel heightLabel = new JLabel("Height:");
-        private JTextField heightField = new JTextField("480");
+        private JTextField heightField = new JTextField("77");
         private JButton generateMazeButton = new JButton("Generate Maze");
         private JButton DFSButton = new JButton("Solve by DFS");
         private JButton BSFButton = new JButton("Solve by BFS");
@@ -41,8 +65,25 @@ public class MazeFrame extends JFrame {
             this.add(generateMazeButton);
             this.add(DFSButton);
             this.add(BSFButton);
+        }
 
+        public String getWidthInput() {
+            return this.widthField.getText();
+        }
 
+        public String getHeightInput() {
+            return this.heightField.getText();
+        }
+        public JButton getGenerateMazeButton() {
+            return this.generateMazeButton;
+        }
+
+        public JButton getDFSButton() {
+            return this.DFSButton;
+        }
+
+        public JButton getBSFButton() {
+            return this.BSFButton;
         }
     }
 
